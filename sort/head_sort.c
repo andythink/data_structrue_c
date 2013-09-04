dd59c67463b59f574b5943f4801b5a0ef6a83345
zjgnlzq <zjgnlzq@126.com>
2013-09-04 09:40:07 +0800

    head sort

--- sort/head_sort.c
+++ sort/head_sort.c
@@ -8,14 +8,43 @@ void adjust(int data[], int i, int length)
 	j = 2*i + 1;
 	while(j <= length)
 	{
-		if()
+		if (j < length && data[j] < data[j+1])
+		{
+			j++;
+		}
+		if(temp > data[j])
+		{
+			break;
+		}
+		data[(j-1)/2] = data[j];
+		j = 2*j + 1;
+	}
+	data[(j-1)/2] = temp;
+}
+
+void headSort(int data[], int length)
+{
+	int i;
+	int temp;
+	for (i=(length/2)-1; i >=0 ; i--)
+	{
+		adjust(data, i, length);
+	}
+
+	for (i=length-2; i >= 0 ; i--)
+	{
+		temp = data[i+1];
+		data[i+1] = data[0];
+		data[0] = temp;
+		adjust(data, 0, i);
 	}
 }
 
-
-
 int main(int argc, char const *argv[])
 {
+	printData();
+	printf("\n");
+	headSort(data, length);
 	printData();
 	return 0;
 }
